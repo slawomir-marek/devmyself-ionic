6e86b009fb8e90ab0de295538717f2aafe2589c8
Sławomir Marek <slawomir.marek@gmail.com>
2023-07-06 15:06:54 +0200

    chore: replace testing framework (karma -> jest)

---
+++ jest.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from 'jest/build';
+
+const config: Config = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+};
+
+export default config;

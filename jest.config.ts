09823ccc8cb736da1c5866a0d73c8776b609e762
Sławomir Marek <slawomir.marek@gmail.com>
2023-08-11 12:33:09 +0200

    chore: update list and test settings

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,16 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  transformIgnorePatterns: [
+    'node_modules/(?!(@angular|@ngrx|@ionic-native|@ionic))',
+    '<roodDir>/node_modules/(?!@ionic-enterprise)',
+  ],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1',
+    '^@ionic/core/dist/ionic/svg': '<rootDir>/src/svgoverride.js',
+  },
+  testPathIgnorePatterns: ['plugins/'],
 };
 
 export default config;

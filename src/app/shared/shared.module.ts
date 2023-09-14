ca0c89c04ad393c620e0bf6f46968546836e333c
Sławomir Marek <slawomir.marek@gmail.com>
2023-09-14 10:02:35 +0200

    fix: update shared domain definition

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 
+import { TileItemComponent, TilesGridComponent, TilesSliderComponent } from './components';
 import { NgLetDirective } from './directives';
 
 @NgModule({
-  imports: [CommonModule],
-  exports: [NgLetDirective],
-  declarations: [NgLetDirective],
+  imports: [CommonModule, IonicModule],
+  exports: [IonicModule, NgLetDirective, TileItemComponent, TilesSliderComponent, TilesGridComponent],
+  declarations: [NgLetDirective, TileItemComponent, TilesSliderComponent, TilesGridComponent],
   providers: [],
 })
 export class SharedModule {}
